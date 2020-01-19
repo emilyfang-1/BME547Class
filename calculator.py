e96c6803a44965d14d6d0e1fada98b75c6e6d150
Emily <emily.fang@duke.edu>
2020-01-19 18:30:21 -0500

    Added option for user to select operation and two numbers

--- calculator.py
+++ calculator.py
@@ -2,31 +2,43 @@
 
 def add(x, y):
     z = x + y
-    return z
+    symbol = "+"
+    return z, symbol
 
 def sub(x, y):
     z = x - y
-    print("{} - {} = {}".format(x, y, z))
+    symbol = "-"
+    return z, symbol
 
 def multiply(x, y):
     z = x * y
-    print("{} * {} = {}".format(x, y, z))
+    symbol = "*"
+    return z, symbol
 
 def divide(x, y):
     z = x / y
-    
+    symbol = "/"
+    return z, symbol
     
 x = input("Enter a letter: ")
-print("You entered {}".format(x))
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+num1 = float(num1)
+num2 = float(num2)
+
 if x == "a":
-    z = add(47,7)
-    print("{} + {} = {}".format(47, 7, z))
+    z, sym = add(num1, num2)
+    print("{} {} {} = {}".format(num1, sym, num2, z))  
 elif x == "s":
-    sub(47, 7)
+    z, sym = sub(num1, num2)
+    print("{} {} {} = {}".format(num1, sym, num2, z))
 elif x == "m":
-    multiply(47, 7)
+    z,sym = multiply(num1, num2)
+    print("{} {} {} = {}".format(num1, sym, num2, z))
 elif x == "d":
-    divide(47, 4)
+    z, sym = divide(num1, num2)
+    print("{} {} {} = {}".format(num1, sym, num2, z))
 else:
     print("The {} command is not recognized".format(x))
+
 print("Done")
